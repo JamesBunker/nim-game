5814b0ac34f203067bc3d69a96b7a01ae47c154d
James Bunker <97250900+JamesBunker@users.noreply.github.com>
2022-01-06 15:41:19 -0500

    Update nimhelp.c

--- nimgame/nimhelp.c
+++ nimgame/nimhelp.c
@@ -1,16 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: Colvin
- *
- * Created on November 22, 2021, 3:18 p.m.
- */
-
+//Coded by Stefan Kremer
 
 #include "nim.h"
 
